131bacea1f468864677e7ff7f468938c830f5d1e
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2022-04-21 17:26:47 -0300

    Criando formulário

--- cupido-site/src/App.js
+++ cupido-site/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
+import './App.css';;
+ 
 function App() {
+  const [campos, setCampos] = useState({
+    nome: '',
+    email: '',
+    mensagem: '',
+    anexo: ''
+  });
+
+  function handleInputChange(event){
+    if(event.target.name === "anexo")
+      campos[event.target.name] = event.target.files[0];
+    else
+      campos[event.target.name] = event.target.value;
+    setCampos(campos);
+  }
+
+  function handleFormSubmit(event){
+      event.preventDefault();
+      console.log(campos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="email">E-mail</label>
+        <input type="text" id="email" name="email" placeholder="E-mail de destino.." onChange={handleInputChange}/>
+ 
+        <label htmlFor="nome">Nome</label>
+        <input type="text" id="nome" name="nome" placeholder="Nome da pessoa.." onChange={handleInputChange}/>
+ 
+        <label htmlFor="mensagem">Mensagem</label>
+        <textarea id="mensagem" name="mensagem" placeholder="Escreva algo.." className="textArea" onChange={handleInputChange}></textarea>
+ 
+        <label htmlFor="anexo">Anexo</label>
+        <input type="file" id="anexo" name="anexo" onChange={handleInputChange}/>
+ 
+        <input type="submit" value="Enviar" />
+      </form>
     </div>
   );
 }
-
+ 
 export default App;
